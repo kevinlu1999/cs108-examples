8caa663f74927774e7085d5fef7f5dbc030dc0fb
kevinlu1999 <kevinlu@bu.edu>
2021-10-28 14:22:38 +0800

    Added pages application with templated HTML

--- cs108/urls.py
+++ cs108/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # add this line into the list of urlpatterns
     path('', include('hello_world.urls')),
+    path('pages/', include('pages.urls')),  # add this new URL include
 ]
